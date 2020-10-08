6f4c2910283a5ef1ca796479ef8c8eac519aed90
Equan Pr <equan.p@gmail.com>
2020-10-08 16:59:25 +0700

    format :)

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import { HijriyahKeMasehi, MasehikeHijriyah } from "./util/index";
 
 function JawaKeHijriyah({ d, m, y }) {
-  return { tgl:d, bln:m, thn: y - 512 };
+  return { tgl: d, bln: m, thn: y - 512 };
 }
 
 function HijriyahKeJawa({ d, m, y }) {
-  return { tgl:d, bln:m, thn: y + 512 };
+  return { tgl: d, bln: m, thn: y + 512 };
 }
 
-module.exports = { JawaKeHijriyah, HijriyahKeJawa, MasehikeHijriyah, HijriyahKeMasehi };
+module.exports = {
+  JawaKeHijriyah,
+  HijriyahKeJawa,
+  MasehikeHijriyah,
+  HijriyahKeMasehi,
+};
