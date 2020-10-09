8699900a7303627461c33ce87fdf1180c9219386
Equan Pr <equan.p@gmail.com>
2020-10-09 14:32:25 +0700

    testing

--- util/__tests__/_index.test.js
+++ util/__tests__/_index.test.js
@@ -1,27 +1,29 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 import * as Konversi from "../../index";
 import * as M2H from "../m2h";
 
 describe("Test All Konversi", () => {
-  test("Masehi Ke Hijriyah", () => {
-    let h = Konversi.MasehikeHijriyah(2020, 10, 7);
-    expect(h).toStrictEqual({ hy: 1442, hm: 2, hd: 19 });
-  });
-
   test("Hijriyah Ke Masehi", () => {
-    let m = Konversi.HijriyahKeMasehi(1442, 2, 19);
-    expect(m).toStrictEqual({ gy: 2020, gm: 10, gd: 7 });
+    let m = Konversi.HijriyahKeMasehi(1442, 2, 21);
+    expect(m).toStrictEqual({ gy: 2020, gm: 10, gd: 9 });
   });
 
   test("Masehi ke Hijriyah M2H", () => {
-    let x = M2H.konversi(new Date(), -2);
-    console.log(x);
+    let { year, month, dayOfMonth } = M2H.MasehiKeHijriyah( new Date("2020-10-7"), -1);
+    expect(year).toBe(1442);
+    expect(month).toBe(2);
+    expect(dayOfMonth).toBe(19);
   });
 
   test("Jawa ke Hijriyah", () => {
-    let j = Konversi.JawaKeHijriyah({d:19, m:2, y:1954})
-    console.log(j)
-  })
+    let j = Konversi.JawaKeHijriyah({ d: 19, m: 2, y: 1954 });
+    expect(j).toEqual({ tgl: 19, bln: 2, thn: 1442 });
+  });
+
+  test("Masehi Ke Jawa", () => {
+    let o = Konversi.MasehiKeJawa({ y: 2020, m: 10, d: 18 });
+    expect(o).toEqual({ tgl: 1, bln: 3, thn: 1954 });
+  });
 });
