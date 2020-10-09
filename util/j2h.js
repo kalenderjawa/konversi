5f0a0b3194df0827a11dff4ef718db142b3d692a
Equan Pr <equan.p@gmail.com>
2020-10-09 14:32:10 +0700

    Tambahkan fungsi konversi Masehi ke Jawa

--- util/j2h.js
+++ util/j2h.js
@@ -1,6 +1,7 @@
 /**
  * Jawa Util
  */
+import { MasehiKeHijriyah } from "./m2h";
 
 function JawaKeHijriyah({ d, m, y }) {
   return { tgl: d, bln: m, thn: y - 512 };
@@ -10,4 +11,12 @@ function HijriyahKeJawa({ d, m, y }) {
   return { tgl: d, bln: m, thn: y + 512 };
 }
 
-export { JawaKeHijriyah, HijriyahKeJawa };
+function MasehiKeJawa({ y, m, d }) {
+  let { year, month, dayOfMonth } = MasehiKeHijriyah(
+    new Date(`${y}-${m}-${d}`),
+    -1
+  );
+  return HijriyahKeJawa({ d: dayOfMonth, m: month, y: year });
+}
+
+export { JawaKeHijriyah, HijriyahKeJawa, MasehiKeJawa };
